dc1b437f3b5d4047df8898ee53135abc16f3769f
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-10 22:02:04 +0300

    added the universal head content file

---
+++ includes/head-content.php
@@ -0,0 +1,20 @@
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="author" content="Bevis Africa">
+<meta name="description" content="Bevis Africa Website">
+
+<!-- Favicon -->
+<link rel="shortcut icon" href="assets/images/favicon.ico">
+
+<!-- Google Font -->
+<link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800,900%7CPlayfair+Display:400,400i,700,700i%7CRoboto:400,400i,500,700" rel="stylesheet">
+
+<!-- Plugins CSS -->
+<link rel="stylesheet" type="text/css" href="assets/vendor/font-awesome/css/all.min.css" />
+<link rel="stylesheet" type="text/css" href="assets/vendor/themify-icons/css/themify-icons.css" />
+<link rel="stylesheet" type="text/css" href="assets/vendor/animate/animate.min.css" />
+<link rel="stylesheet" type="text/css" href="assets/vendor/glightbox/css/glightbox.css">
+
+<!-- Theme CSS -->
+<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
