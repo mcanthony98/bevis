d81bbf41072105aa61efbc0e7b89516613e5af8d
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-10 22:10:00 +0300

    added the universal footer sections

---
+++ includes/footer.php
@@ -0,0 +1,102 @@
+	<!-- =======================
+	call to action-->
+	<section class="p5-4">
+	    <div class="container">
+	        <div class="d-block d-md-flex bg-grad p-4 p-sm-5 all-text-white rounded pattern-overlay-3">
+	            <div class="align-self-center text-center text-md-start">
+	                <h3>Crafting visually stunning memorable experiences for web.</h3>
+	                <p class="m-0">We provide digital transformation solutions, enabling companies to make their visions reality.</p>
+	            </div>
+	            <div class="mt-3 mt-md-0 text-center text-md-end ms-md-auto align-self-center">
+	                <button class="btn btn-white mb-0">Learn more!</button>
+	            </div>
+	        </div>
+	    </div>
+	</section>
+	<!-- =======================
+	call to action-->
+
+	<!-- =======================
+	footer  -->
+	<footer class="footer bg-light pt-6">
+	    <div class="footer-content pb-3">
+	        <div class="container">
+	            <div class="row">
+	                <!-- Footer widget 1 -->
+	                <div class="col-md-4">
+	                    <div class="widget">
+	                        <a href="index.php" class="footer-logo">
+	                            <!-- footer logo Start -->
+	                            <img class="footer-logo-item" src="assets/images/logo.svg" alt="">
+	                            <!-- footer logo End -->
+	                        </a>
+	                        <p class="mt-3">Wizixo is a highly creative, modern, visually stunning and Bootstrap 5 Creative Multi-purpose corporate and portfolio HTML5 template.</p>
+	                        <p>It is based on the powerful Bootstrap 5 Framework to make sure an entirely responsive layout. </p>
+	                    </div>
+	                </div>
+	                <!-- Footer widget 2 -->
+	                <div class="col-md-3 col-sm-6">
+	                    <div class="widget address" style="background-image: url('assets/images/world-map.png'); background-position: 50% 20px; background-repeat: no-repeat; background-size: contain;">
+	                        <ul class="list-group list-group-borderless">
+	                            <li class="d-flex mb-3"><i class="me-3 display-8 ti-map-alt"></i>1421 Coburn Hollow Road Metamora, Near Center Point, IL 61548. </li>
+	                            <li class="d-flex mb-3"><i class="me-3 display-8 ti-headphone-alt"></i> (251) 854-6308 </li>
+	                            <li class="d-flex mb-3"><i class="me-3 display-8 ti-email"></i> help@wizixo.com</li>
+	                            <li class="d-flex mb-3"><i class="me-3 display-8 ti-time"></i>
+	                                <p>Mon - Fri: <strong>09:00 - 21:00</strong> <br>Sat & Sun: <strong>Closed</strong></p>
+	                            </li>
+	                        </ul>
+	                    </div>
+	                </div>
+	                <!-- Footer widget 3 -->
+	                <div class="col-md-2 col-sm-6">
+	                    <div class="widget">
+	                        <h6>Quick LInks</h6>
+	                        <ul class="nav flex-column primary-hover">
+	                            <li class="nav-item"><a class="nav-link" href="about-classic.php">About</a></li>
+	                            <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+	                            <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+	                            <li class="nav-item"><a class="nav-link" href="blog-grid-left-sidebar.php">Blog</a></li>
+	                            <li class="nav-item"><a class="nav-link" href="portfolio-grid-column-4.php">Portfolio</a></li>
+	                        </ul>
+	                    </div>
+	                </div>
+	                <!-- Footer widget 4 -->
+	                <div class="col-md-3">
+	                    <div class="widget bg-grad p-4 all-text-white rounded">
+	                        <i class="ti-email newsletter-icon"></i>
+	                        <p class="mb-2">Subscribe to our newsletter to receive exclusive offers.</p>
+	                        <div class="form-group mb-0">
+	                            <input class="form-control border-white mb-3" type="email" placeholder="Enter email...">
+	                            <button class="btn btn-dark mb-0">Join us</button>
+	                        </div>
+	                    </div>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    <div class="divider mt-3"></div>
+	    <!--footer copyright -->
+	    <div class="footer-copyright py-3">
+	        <div class="container">
+	            <div class="d-md-flex justify-content-between align-items-center py-3 text-center text-md-start">
+	                <!-- copyright text -->
+	                <div class="copyright-text">©2021 All Rights Reserved by <a href="#!"> Wizixo.</a></div>
+	                <!-- copyright links-->
+	                <div class="copyright-links primary-hover mt-3 mt-md-0">
+	                    <ul class="list-inline">
+	                        <li class="list-inline-item ps-2"><a class="list-group-item-action" href="#">Home</a></li>
+	                        <li class="list-inline-item ps-2"><a class="list-group-item-action" href="#">About Us</a></li>
+	                        <li class="list-inline-item ps-2"><a class="list-group-item-action" href="#">Career</a></li>
+	                        <li class="list-inline-item ps-2"><a class="list-group-item-action" href="#">Privacy Policy</a></li>
+	                        <li class="list-inline-item ps-2"><a class="list-group-item-action pe-0" href="#">Use of terms</a></li>
+	                    </ul>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	</footer>
+	<!-- =======================
+	footer  -->
+
+	<!-- Back to top -->
+	<div> <a href="#" class="back-top btn btn-grad"><i class="ti-angle-up"></i></a> </div>
