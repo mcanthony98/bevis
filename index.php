4887bd4e1287c55d0b041cf9290b1f434f099522
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-10 23:16:32 +0300

    added the homepage

---
+++ index.php
@@ -0,0 +1,72 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Home | Bevis Africa</title>
+
+    <!-- Head Content -->
+    <?php include 'includes/head-content.php'; ?>
+
+
+</head>
+
+<body>
+    <div class="preloader">
+        <img src="assets/images/preloader.svg" alt="Pre-loader">
+    </div>
+
+    <!-- Header -->
+    <?php include 'includes/navbar.php'; ?>
+
+    <!-- =======================
+	Main Banner -->
+    <section class="p-0 position-relative h-500 h-md-600 h-xl-900 bg-grad pattern-overlay-1 overflow-hidden">
+        <div class="container d-flex h-100">
+            <div class="row align-self-center w-100">
+                <!-- banner Content -->
+                <div class="col-md-6 mt-md-0 all-text-white position-relative">
+                    <h2 class="display-4 fw-normal">A complete technology solution provider</h2>
+                    <p class="h6 mb-4 fw-normal d-none d-sm-block">We help our customers better manage their web presence in order to achieve greater success online.</p>
+                    <a class="btn btn-dark mb-7" href="#">Request demo!</a>
+                </div>
+                <!-- banner image -->
+                <div class="col-md-6 d-none d-md-block p-0 position-absolute top-0 end-0 align-top align-text-top">
+                    <figure class="w-100 ie-height-750">
+                        <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1290 1024" style="enable-background:new 0 0 1290 1024;" xml:space="preserve">
+                            <g id="XMLID_81_">
+                                <defs>
+                                    <path id="XMLID_3_" d="M1290,0v1024c0,0-229.8-152.2-550-52.9C467.3,1055.7-22.5,927.6,47.8,540C120.4,139.6,17,0,17,0H1290z" />
+                                </defs>
+                                <clipPath id="XMLID_83_">
+                                    <use xlink:href="#XMLID_3_" style="overflow:visible;" />
+                                </clipPath>
+                                <g style="clip-path:url(#XMLID_83_);">
+                                    <!-- Set your 1290px X 1024px image below -->
+                                    <image style="overflow:visible;" width="1290" height="1024" id="XMLID_82_" xlink:href="assets/images/banner/10.jpg" transform="matrix(0.9999 0 0 0.9999 8.063897e-002 6.401104e-002)">
+                                    </image>
+                                </g>
+                            </g>
+                        </svg>
+                    </figure>
+                </div>
+            </div>
+        </div>
+        <canvas class="position-absolute bottom-0 start-50 translate-middle-x mb-n9" id="waveCanvas"></canvas>
+    </section>
+    <!-- =======================
+	Main banner -->
+
+
+
+
+    <!-- Footer, CTA -->
+    <?php include 'includes/footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <?php include 'includes/scripts.php'; ?>
+
+
+</body>
+
+</html>
