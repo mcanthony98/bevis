7fe9881588ef1006d53b0b8d52f2a52eb08f94ff
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-14 21:19:54 +0300

    added the about us section

--- about-us.php
+++ about-us.php
@@ -34,7 +34,64 @@
 	Banner innerpage -->
 
 
-    
+    	<!-- =======================
+	About us -->
+	<section>
+		<div class="container h-100">
+			<div class="row">
+				<div class="col-md-12 col-lg-6 align-self-center ">
+					<div class="title text-start">
+						<span class="pre-title">We are the Wizixo!</span>
+						<h2>We are a full service <span class="text-primary">Creative agency</span> that makes good ideas come to life.</h2>
+						<p class="mb-0">We are an insight and behaviour driven creative marketing agency. A Full package Digital Creative Agency Specializing in: Video Production, Web Design, Branding, Brand Strategy, Content Marketing and Aerial Cinematography. </p>
+						<div class="list-group-inline list-group-number list-unstyled mt-4">
+							<a href="#" class="list-group-item list-group-item-action"><span>01</span> Creative Ideas</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>02</span> Super Responsive for all devices</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>03</span> Extensive documentation</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>04</span> Excellent features</a>
+						</div>
+					</div>
+				</div>
+				<div class="col-md-10 col-lg-6 mx-md-auto align-self-center position-relative">
+					<img class="rounded" src="assets/images/blog/02.jpg" alt="">
+					<div class="position-absolute start-0 bottom-0 ms-4 ms-md-n2 mb-3">
+						<a class="btn btn-grad"  data-glightbox href="https://youtu.be/n_Cn8eFo7u8"> <i class="fa fa-play text-white"></i>Play Video </a>
+					</div>
+				</div>
+			</div>
+			<!-- row end -->
+
+			<div class="row mt-4">
+				<!-- feature 1 -->
+				<div class="col-md-4">
+					<div class="feature-box h-100 icon-primary">
+						<div class="feature-box-icon"><i class="ti-settings"></i></div>
+						<h3 class="feature-box-title">Unlimited Helpers</h3>
+						<p class="feature-box-desc">We have vast collection of sed do eiusmod tempor incididunt ut labore et <strong>Dolore magna aliqua.</strong> </p>
+					</div>
+				</div>
+				<!-- feature 2 -->
+				<div class="col-md-4">
+					<div class="feature-box h-100 icon-primary">
+						<div class="feature-box-icon"><i class="ti-support"></i></div>
+						<h3 class="feature-box-title">Friendly Support</h3>
+						<p class="feature-box-desc">With this template you can incididunt ut labore et dolore magna aliqua ad minim veniam quis ullamco.</p>
+					</div>
+				</div>
+				<!-- feature 3 -->
+				<div class="col-md-4">
+					<div class="feature-box h-100 icon-primary">
+						<div class="feature-box-icon"><i class="ti-bolt"></i></div>
+						<h3 class="feature-box-title">Instant Solutions</h3>
+						<p class="feature-box-desc">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <strong>$255.00</strong>dolor in reprehe. </p>
+					</div>
+				</div>
+			</div>
+			<!-- row end -->
+		</div>
+	</section>
+	<!-- =======================
+	About us -->
 
 
 
