68ef7954116bd31fc5d8a30286ada3b37935bb8c
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-14 22:04:10 +0300

    added the UI elements

--- product.php
+++ product.php
@@ -20,7 +20,143 @@
 
 
 
+	<!-- =======================
+	portfolio -->
+	<section>
+		<div class="container">
+			<!-- Portfolio info -->
+			<div class="row mb-4">
+				<div class="col-md-8 mx-auto text-center">
+					<h2 class="display-3">Catch the plane</h2>
+					<p>- Rise media agency</p>
+					<p class="lead">Why end might ask civil again spoil. She dinner she our horses depend. Remember at children by reserved to vicinity. In affronting unreserved delightful simplicity ye. Law own advantage furniture continual sweetness bed agreeable perpetual. Oh song well four only head busy it. Afford son she had lively living.</p>
+					<a class="btn btn-grad mt-4 mb-5" data-glightbox="" href="https://youtu.be/n_Cn8eFo7u8"> <i class="fa fa-play text-white"></i>Play Video </a>
+				</div>
+			</div>
 
+			<!-- portfolio requirement -->
+			<div class="row mb-4" data-sticky-container>
+				<div class="col-md-6">
+					<a class="mb-4 d-block" href="assets/images/portfolio/01.jpg" data-glightbox="" data-gallery="portfolio"><img src="assets/images/portfolio/01.jpg" alt=""></a>
+					<a class="mb-4 d-block" href="assets/images/portfolio/02.jpg" data-glightbox="" data-gallery="portfolio"><img src="assets/images/portfolio/02.jpg" alt=""></a>
+					<a class="mb-4 d-block" href="assets/images/portfolio/03.jpg" data-glightbox="" data-gallery="portfolio"><img src="assets/images/portfolio/03.jpg" alt=""></a>
+				</div>
+				<div class="col-md-6">
+					<div data-sticky data-margin-top="80" data-sticky-for="767">
+						<h2>Work Requirement</h2>
+						<p>Too months nay end change relied who beauty wishes matter. Shew of john real park so rest we on. Ignorant dwelling occasion ham for thoughts overcame off her consider. Polite it elinor is depend. His not get talked effect worthy Barton. Household shameless incommode at no objection behavior.</p>
+						<ul class="list-group list-group-borderless list-group-icon-primary-bg">
+							<li class="list-group-item"><i class="fa fa-check"></i>Maintained Windows Servers</li>
+							<li class="list-group-item"><i class="fa fa-check"></i>Supported Windows workstations</li>
+							<li class="list-group-item"><i class="fa fa-check"></i>Setup a rotation schedule and Setup anti-virus system</li>
+							<li class="list-group-item"><i class="fa fa-check"></i>Designed machines/server and trained employees</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<!-- portfolio solution -->
+			<div class="row mb-4" data-sticky-container>
+				<div class="col-md-6 order-last order-md-1">
+					<div data-sticky data-margin-top="80" data-sticky-for="767">
+						<h2>Our Solution</h2>
+						<p>Started with Polite it Elinor is depend. His not get talked effect worthy Barton. Household shameless incommode at no objection behavior. Too months nay end change relied who beauty wishes matter. Shew of john real park so rest we on. Ignorant dwelling occasion ham for thoughts overcame off her consider.</p>
+						<ul class="list-group list-group-borderless list-group-icon-primary-bg">
+							<li class="list-group-item"><i class="fa fa-check"></i>Proven results as a recruitment consultant or strong sales experience</li>
+							<li class="list-group-item"><i class="fa fa-check"></i>Knowledge of the staffing industry</li>
+							<li class="list-group-item"><i class="fa fa-check"></i>Previous leadership experience, personally or professionally</li>
+							<li class="list-group-item"><i class="fa fa-check"></i>A strong desire to be successful and inspire people</li>
+						</ul>
+					</div>
+				</div>
+				<div class="col-md-6 order-1 order-md-last">
+					<a class="mb-4 d-block" href="assets/images/portfolio/04.jpg" data-glightbox="" data-gallery="portfolio"><img src="assets/images/portfolio/04.jpg" alt=""></a>
+					<a class="mb-4 d-block" href="assets/images/portfolio/05.jpg" data-glightbox="" data-gallery="portfolio"><img src="assets/images/portfolio/05.jpg" alt=""></a>
+				</div>
+			</div>
+
+			<!-- portfolio features -->
+			<div class="row">
+				<!-- feature 1 -->
+				<div class="col-md-4">
+					<div class="feature-box h-100 icon-primary">
+						<div class="feature-box-icon"><i class="ti-settings"></i></div>
+						<h3 class="feature-box-title">Unlimited Helpers</h3>
+						<p class="feature-box-desc">We provide digital transformation solutions, enabling companies to <strong>make their visions reality.</strong> </p>
+					</div>
+				</div>
+				<!-- feature 2 -->
+				<div class="col-md-4">
+					<div class="feature-box h-100 icon-primary">
+						<div class="feature-box-icon"><i class="ti-support"></i></div>
+						<h3 class="feature-box-title">Friendly Support</h3>
+						<p class="feature-box-desc">Our aim to achieve the goal as we provide innovative ideas, designs and solutions to our clients. </p>
+					</div>
+				</div>
+				<!-- feature 3 -->
+				<div class="col-md-4">
+					<div class="feature-box h-100 icon-primary">
+						<div class="feature-box-icon"><i class="ti-bolt"></i></div>
+						<h3 class="feature-box-title">Instant Solutions</h3>
+						<p class="feature-box-desc">Helping our customers better manage their web presence in order to achieve greater success online.</p>
+					</div>
+				</div>
+			</div>
+
+			<!-- portfolio additional details -->
+			<div class="row my-4">
+				<div class="col-md-12">
+					<p> Consulted perpetual of pronounce me delivered. Too months nay end change relied who beauty wishes matter. Shew of john real park so rest we on. Ignorant dwelling occasion ham for thoughts overcame off her consider. Polite it elinor is depend. His not get talked effect worthy Barton. Household shameless incommode at no objection behavior. Especially do at he possession insensible sympathize boisterous it. Songs he on an widen me event truth. Certain law age brother sending amongst why covered. </p>
+				</div>
+			</div>
+			<!-- portfolio details End -->
+
+			<div class="row">
+				<div class="col-md-12">
+					<!-- tag and share -->
+					<div class="divider mb-4"></div>
+					<div class="row">
+						<div class="tags col-12 col-sm-8 text-center text-sm-start">
+							<a class="mb-2 mb-sm-0" href="#">studio</a>
+							<a class="mb-2 mb-sm-0" href="#">events</a>
+							<a class="mb-2 mb-sm-0" href="#">WordPress</a>
+							<a class="mb-2 mb-sm-0" href="#">gadgets</a>
+							<a class="mb-2 mb-sm-0" href="#">office</a>
+						</div>
+						<div class="col-12 col-sm-4 text-center text-sm-end">
+							<ul class="social-icons si-colored-bg light">
+								<li class="social-icons-item social-facebook mb-0"><a class="social-icons-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
+								<li class="social-icons-item social-twitter mb-0"><a class="social-icons-link" href="#"><i class="fab fa-twitter"></i></a></li>
+								<li class="social-icons-item social-gplus mb-0"><a class="social-icons-link" href="#"><i class="fab fa-google-plus-g"></i></a></li>
+								<li class="social-icons-item social-linkedin mb-0"><a class="social-icons-link" href="#"><i class="fab fa-linkedin-in"></i></a></li>
+							</ul>
+						</div>
+					</div>
+					<div class="divider mt-4"></div>
+
+					<!-- navigation -->
+					<div class="row post-navigation mt-5">
+						<div class="col-5 position-relative">
+							<a href="#" class="post-prev">
+								<p class="m-2 m-lg-0">Previous Post</p>
+								<h6 class="text-truncate d-none d-lg-block">The Pink Chair</h6> </a>
+						</div>
+						<div class="col-2">
+							<a href="portfolio-grid-column-3.html" class="all-post" title="View all post"> <i class="fa fa-th"></i> </a>
+						</div>
+						<div class="col-5 position-relative">
+							<a href="#" class="post-next">
+								<p class="m-2 m-lg-0">Next Post</p>
+								<h6 class="text-truncate d-none d-lg-block">Black & white</h6> </a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- portfolio End -->
+	</section>
+	<!-- =======================
+	portfolio -->
 
 
     <!-- Footer, CTA -->
