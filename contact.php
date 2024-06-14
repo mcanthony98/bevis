11783e94a8685ec7655c823c6cc6b32cfcaffe39
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-14 21:59:30 +0300

    added the banner section

--- contact.php
+++ contact.php
@@ -19,7 +19,21 @@
     <?php include 'includes/navbar.php'; ?>
 
 
-
+	<!-- =======================
+	Banner innerpage -->
+	<div class="text-center bg-overlay-dark-4" style="background:url(assets/images/bg/05.jpg) no-repeat; background-size: cover; background-position: center center;">
+		<div class="container">
+			<div class="row all-text-white">
+				<div class="col-md-12 align-self-center">
+					<h6 class="display-6 fw-bold mt-9 mb-0">Call us at (+251) 854-6308</h6>
+					<h1 class="fw-bold display-1 mb-2 mb-md-n4">Contact Us</h1>
+					<br>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- =======================
+	Banner innerpage -->
 
 
 
