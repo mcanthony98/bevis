95f0c0f08f6412a94996fdad8bf0e278cfc48120
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-14 21:44:32 +0300

    added the services section

--- services.php
+++ services.php
@@ -34,6 +34,60 @@
 	Banner innerpage -->
 
 
+    	<!-- =======================
+	service -->
+	<section class="service">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6 col-md-4">
+					<div class="feature-box h-100 icon-grad">
+						<div class="feature-box-icon"><i class="ti-panel"></i></div>
+						<h3 class="feature-box-title">Super-easy customizing</h3>
+						<p class="feature-box-desc">We have vast collection of sed do eiusmod tempor incididunt ut labore et <strong>Dolore magna aliqua.</strong> Ut enim ad minim veniam, quis nostrud.</p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="feature-box h-100 icon-grad">
+						<div class="feature-box-icon"><i class="ti-package"></i></div>
+						<h3 class="feature-box-title">Premium Plugins</h3>
+						<p class="feature-box-desc">With this template you can incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="feature-box h-100 icon-grad">
+						<div class="feature-box-icon"><i class="ti-credit-card"></i></div>
+						<h3 class="feature-box-title">Budget accounting</h3>
+						<p class="feature-box-desc">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <strong>$255.00</strong> pm Duis aute irure dolor in reprehenderit in voluptate velit. </p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="feature-box h-100 icon-grad">
+						<div class="feature-box-icon"><i class="ti-layout-slider"></i></div>
+						<h3 class="feature-box-title">Work flow automation</h3>
+						<p class="feature-box-desc">We have vast collection of sed do eiusmod tempor incididunt ut labore et <strong>Dolore magna aliqua.</strong> Ut enim ad minim veniam, quis nostrud.</p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="feature-box h-100 icon-grad">
+						<div class="feature-box-icon"><i class="ti-wallet"></i></div>
+						<h3 class="feature-box-title">Solution Defining</h3>
+						<p class="feature-box-desc">With this template you can incididunt ut labore et dolore magna aliqua Ut enim ad minim veniam quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
+					</div>
+				</div>
+				<div class="col-sm-6 col-md-4">
+					<div class="feature-box h-100 icon-grad">
+						<div class="feature-box-icon"><i class="ti-share-alt"></i></div>
+						<h3 class="feature-box-title">High Performance</h3>
+						<p class="feature-box-desc">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <strong>$255.00</strong> pm Duis aute irure dolor in reprehenderit in voluptate velit. </p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- =======================
+	service -->
+
+
     <!-- Footer, CTA -->
     <?php include 'includes/footer.php'; ?>
 
