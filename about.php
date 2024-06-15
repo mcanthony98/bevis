dc5f9ffef1744c148cccfef13c25f609ad0f0f44
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-15 04:11:04 +0300

    changed the content

--- about.php
+++ about.php
@@ -2,53 +2,53 @@
 <html lang="en">
 
 <head>
-    <title>About Us | Bevis Africa</title>
+	<title>About Us | Bevis Africa</title>
 
-    <!-- Head Content -->
-    <?php include 'includes/head-content.php'; ?>
+	<!-- Head Content -->
+	<?php include 'includes/head-content.php'; ?>
 
 
 </head>
 
 <body>
-    <div class="preloader">
-        <img src="assets/images/preloader.svg" alt="Pre-loader">
-    </div>
+	<div class="preloader">
+		<img src="assets/images/preloader.svg" alt="Pre-loader">
+	</div>
 
-    <!-- Header -->
-    <?php include 'includes/navbar.php'; ?>
+	<!-- Header -->
+	<?php include 'includes/navbar.php'; ?>
 
 	<!-- =======================
-	Banner innerpage -->
+Banner innerpage -->
 	<div class="left bg-grad pattern-overlay-4">
 		<div class="container">
 			<div class="row all-text-white">
 				<div class="col-md-12 align-self-center position-relative">
-					<h1 class="fw-bold display-1 mb-2 mb-md-n4 mt-9">About Wizixo</h1>
+					<h1 class="fw-bold display-1 mb-2 mb-md-n4 mt-9">About Bevis Africa</h1>
 					<br>
 				</div>
 			</div>
 		</div>
 	</div>
 	<!-- =======================
-	Banner innerpage -->
+Banner innerpage -->
 
 
-    	<!-- =======================
-	About us -->
+	<!-- =======================
+About us -->
 	<section>
 		<div class="container h-100">
 			<div class="row">
 				<div class="col-md-12 col-lg-6 align-self-center ">
 					<div class="title text-start">
-						<span class="pre-title">We are the Wizixo!</span>
-						<h2>We are a full service <span class="text-primary">Creative agency</span> that makes good ideas come to life.</h2>
-						<p class="mb-0">We are an insight and behaviour driven creative marketing agency. A Full package Digital Creative Agency Specializing in: Video Production, Web Design, Branding, Brand Strategy, Content Marketing and Aerial Cinematography. </p>
+						<span class="pre-title">We are Bevis Africa!</span>
+						<h2>We are a <span class="text-primary">Climate Fintech Company</span> focused on accelerating regenerative finance and reducing carbon emissions.</h2>
+						<p class="mb-0">Our mission is to revolutionize climate resilience by leveraging technology to empower small-scale farmers, fishermen, artisanal miners, and pastoralists in greenhouse gas emission reduction across Africa.</p>
 						<div class="list-group-inline list-group-number list-unstyled mt-4">
-							<a href="#" class="list-group-item list-group-item-action"><span>01</span> Creative Ideas</a>
-							<a href="#" class="list-group-item list-group-item-action"><span>02</span> Super Responsive for all devices</a>
-							<a href="#" class="list-group-item list-group-item-action"><span>03</span> Extensive documentation</a>
-							<a href="#" class="list-group-item list-group-item-action"><span>04</span> Excellent features</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>01</span> Regenerative Agriculture</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>02</span> Blue Carbon Ecosystems</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>03</span> Climate-Smart Pasture Management</a>
+							<a href="#" class="list-group-item list-group-item-action"><span>04</span> Voluntary Carbon Market Access</a>
 						</div>
 					</div>
 				</div>
@@ -63,24 +63,24 @@
 				<div class="col-md-4">
 					<div class="feature-box h-100 icon-primary">
 						<div class="feature-box-icon"><i class="ti-settings"></i></div>
-						<h3 class="feature-box-title">Unlimited Helpers</h3>
-						<p class="feature-box-desc">We have vast collection of sed do eiusmod tempor incididunt ut labore et <strong>Dolore magna aliqua.</strong> </p>
+						<h3 class="feature-box-title">Innovative Technology</h3>
+						<p class="feature-box-desc">We leverage cutting-edge technology to accelerate funding for essential ecosystem restoration efforts and drive climate action.</p>
 					</div>
 				</div>
 				<!-- feature 2 -->
 				<div class="col-md-4">
 					<div class="feature-box h-100 icon-primary">
 						<div class="feature-box-icon"><i class="ti-support"></i></div>
-						<h3 class="feature-box-title">Friendly Support</h3>
-						<p class="feature-box-desc">With this template you can incididunt ut labore et dolore magna aliqua ad minim veniam quis ullamco.</p>
+						<h3 class="feature-box-title">Empowering Communities</h3>
+						<p class="feature-box-desc">Our projects empower small-scale producers to access the voluntary carbon market and enhance their resilience to climate change.</p>
 					</div>
 				</div>
 				<!-- feature 3 -->
 				<div class="col-md-4">
 					<div class="feature-box h-100 icon-primary">
 						<div class="feature-box-icon"><i class="ti-bolt"></i></div>
-						<h3 class="feature-box-title">Instant Solutions</h3>
-						<p class="feature-box-desc">Exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <strong>$255.00</strong>dolor in reprehe. </p>
+						<h3 class="feature-box-title">Sustainable Development</h3>
+						<p class="feature-box-desc">We contribute to the overall mitigation of climate change and promote sustainable development through our innovative climate solutions.</p>
 					</div>
 				</div>
 			</div>
@@ -88,23 +88,23 @@
 		</div>
 	</section>
 	<!-- =======================
-	About us -->
+About us -->
 
-		<!-- =======================
-	About us -->
+	<!-- =======================
+About us -->
 	<section>
 		<div class="container h-100">
 			<div class="row">
 				<div class="col-md-6 align-self-center">
 					<div class="text-center text-sm-center text-md-end mb-4">
-						<h2 class="text-grad display-1 fw-bold mb-0">15+ Years</h2>
-						<span>In market insight and behavior driven creative marketing agency</span>
+						<h2 class="text-grad display-1 fw-bold mb-0">5+ Years</h2>
+						<span>In driving climate action and empowering small-scale producers across Africa</span>
 					</div>
 				</div>
 				<div class="col-md-6 align-self-center text-center text-sm-center text-md-start">
-					<p class="pe-0 pe-lg-5">A Full portfolio Digital Creative Agency Specializing in: Video Production, Web Design, Branding, Brand Strategy, Content Marketing and Aerial Cinematography. sequi, obcaecati temporibus laboriosam</p>
-					<p class="pe-0 pe-lg-5">We are an insight and behaviour driven creative marketing agency deleniti delectus fugiat commodi facere atque molestias sequi obcaecati temporibus laboriosam?</p>
-					<a class="btn btn-outline-light" href="#">Explore services!</a>
+					<p class="pe-0 pe-lg-5">Bevis Africa is dedicated to expanding the reach and impact of climate action through the voluntary carbon market. We onboard small-scale farmers, fishermen, artisanal miners, and pastoralists, providing them with the tools and resources needed to reduce greenhouse gas emissions and enhance their resilience to climate change.</p>
+					<p class="pe-0 pe-lg-5">Our innovative technology and comprehensive strategy are accelerating the funding of essential ecosystem restoration efforts, promoting sustainable development, and contributing to the overall mitigation of climate change.</p>
+					<a class="btn btn-outline-light" href="#">Explore our projects!</a>
 				</div>
 			</div>
 			<!-- row end -->
@@ -112,36 +112,32 @@
 				<!-- process item-->
 				<div class="col-md-3 mb-6">
 					<div class="process">
-						<div class="process-number">01.</div>
-						<h3 class="process-title">Research</h3>
-						<p class="process-content">We start with Research consectetur adipisicing elit. Placeat alias optio repellendus recusandae odio praesentium</p>
+						<h3 class="process-title">01. Identify</h3>
+						<p class="process-content">We identify small-scale producers and assess their potential for greenhouse gas emission reduction and carbon sequestration.</p>
 						<a href="#!">Read More</a>
 					</div>
 				</div>
 				<!-- process item-->
 				<div class="col-md-3 mb-6">
 					<div class="process">
-						<div class="process-number">02.</div>
-						<h3 class="process-title">Design</h3>
-						<p class="process-content">In this step Placeat alias optio repellendus recusandae odio praesentium consectetur adipisicing odio elit </p>
+						<h3 class="process-title">02. Develop</h3>
+						<p class="process-content">We develop carbon offset projects tailored to the specific needs and challenges of each community, ensuring maximum impact and sustainability.</p>
 						<a href="#!">Read More</a>
 					</div>
 				</div>
 				<!-- process item-->
 				<div class="col-md-3 mb-6">
 					<div class="process">
-						<div class="process-number">03.</div>
-						<h3 class="process-title">Develop</h3>
-						<p class="process-content">We build with Developer consectetur adipisicing elit Placeat alias optio repellendus recusandae odio praesentium</p>
+						<h3 class="process-title">03.Implement</h3>
+						<p class="process-content">We implement the projects on the ground, providing training, support, and resources to ensure their success and long-term viability.</p>
 						<a href="#!">Read More</a>
 					</div>
 				</div>
 				<!-- process item-->
 				<div class="col-md-3 mb-6">
 					<div class="process">
-						<div class="process-number">04.</div>
-						<h3 class="process-title">Launch</h3>
-						<p class="process-content">Final step of success consectetur optio adipisicing elit Placeat alias optio repellendus recusandae odio done</p>
+						<h3 class="process-title">04. Monitor</h3>
+						<p class="process-content">We continuously monitor and evaluate the projects to ensure their effectiveness, transparency, and adherence to international standards.</p>
 						<a href="#!">Read More</a>
 					</div>
 				</div>
@@ -150,12 +146,12 @@
 		</div>
 	</section>
 	<!-- =======================
-	About us -->
+About us -->
 
 
-    <div class="container divider mb-4 mt-5"> </div>
+	<div class="container divider mb-4 mt-5"> </div>
 	<!-- =======================
-	Partners -->
+Partners -->
 	<section class="client py-5">
 		<div class="container">
 			<div class="row">
@@ -170,25 +166,25 @@
 							<div class="item"><img class="w-75" src="assets/images/clients/06.png" alt=""></div>
 							<div class="item"><img class="w-75" src="assets/images/clients/07.png" alt=""></div>
 							<div class="item"><img class="w-75" src="assets/images/clients/08.png" alt=""></div>
-						</div>	
+						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</section>
 	<!-- =======================
-	Partners -->
+Partners -->
 
 
-    	<!-- =======================
-	Team -->
+	<!-- =======================
+Team -->
 	<section class="team social-hover team-overlay pb-0">
 		<div class="container">
 			<div class="row">
 				<div class="col-12 col-lg-8 mx-auto">
 					<div class="title text-center">
-						<span class="pre-title">They are all professionals</span>
-						<h2>Creative Team and founders</h2>
+						<span class="pre-title">Meet our team of experts</span>
+						<h2>Bevis Africa Leadership</h2>
 					</div>
 				</div>
 			</div>
@@ -200,9 +196,9 @@
 							<img src="assets/images/team/01.jpg" alt="">
 						</div>
 						<div class="team-desc">
-							<h5 class="team-name">Allen Smith</h5>
-							<span class="team-position">Founder</span>
-							<p>Far concluded not his something extremity. Four we face an he gate.</p>
+							<h5 class="team-name">John Doe</h5>
+							<span class="team-position">CEO and Founder</span>
+							<p>An experienced entrepreneur with a passion for driving sustainable development and climate action across Africa.</p>
 							<ul class="social-icons si-colored-on-hover">
 								<li class="social-icons-item social-facebook"><a class="social-icons-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
 								<li class="social-icons-item social-instagram"><a class="social-icons-link" href="#"><i class="fab fa-instagram"></i></a></li>
@@ -218,9 +214,9 @@
 							<img src="assets/images/team/02.jpg" alt="">
 						</div>
 						<div class="team-desc">
-							<h5 class="team-name">Peter Smith</h5>
-							<span class="team-position">Software Developer</span>
-							<p>Sportsman one bed departure rapturous situation disposing partiality.</p>
+							<h5 class="team-name">Jane Smith</h5>
+							<span class="team-position">CTO</span>
+							<p>A seasoned technology expert with a strong background in developing innovative solutions for climate change mitigation.</p>
 							<ul class="social-icons si-colored-on-hover">
 								<li class="social-icons-item social-facebook"><a class="social-icons-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
 								<li class="social-icons-item social-instagram"><a class="social-icons-link" href="#"><i class="fab fa-instagram"></i></a></li>
@@ -236,9 +232,9 @@
 							<img src="assets/images/team/03.jpg" alt="">
 						</div>
 						<div class="team-desc">
-							<h5 class="team-name">Emma Watson</h5>
-							<span class="team-position">Human Resource</span>
-							<p>Continue delicate as unlocked entirely mr relation diverted in Expect</p>
+							<h5 class="team-name">Mark Johnson</h5>
+							<span class="team-position">Director of Operations</span>
+							<p>An accomplished professional with extensive experience in project management and community engagement.</p>
 							<ul class="social-icons si-colored-on-hover">
 								<li class="social-icons-item social-facebook"><a class="social-icons-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
 								<li class="social-icons-item social-instagram"><a class="social-icons-link" href="#"><i class="fab fa-instagram"></i></a></li>
@@ -254,9 +250,9 @@
 							<img src="assets/images/team/04.jpg" alt="">
 						</div>
 						<div class="team-desc">
-							<h5 class="team-name">Jessica Mores</h5>
-							<span class="team-position">Web Developer</span>
-							<p>Expect no pretty though so genius afraid when of ye snug poor draw.</p>
+							<h5 class="team-name">Sarah Thompson</h5>
+							<span class="team-position">Director of Marketing</span>
+							<p>A creative strategist with a knack for developing impactful campaigns that drive awareness and engagement.</p>
 							<ul class="social-icons si-colored-on-hover">
 								<li class="social-icons-item social-facebook"><a class="social-icons-link" href="#"><i class="fab fa-facebook-f"></i></a></li>
 								<li class="social-icons-item social-instagram"><a class="social-icons-link" href="#"><i class="fab fa-instagram"></i></a></li>
@@ -269,24 +265,24 @@
 			<!-- row end -->
 			<div class="row">
 				<div class="col-12 col-lg-8 mx-auto text-center mt-5">
-					<h6>We are hiring! Join our team Creative Agency Specializing in: Video Production, Web Design, Branding, Brand Strategy.</h6>
-					<a class="btn btn-grad" href="#!">Apply now!</a>
+					<h6>Join us in our mission to revolutionize climate resilience and drive sustainable development across Africa.</h6>
+					<a class="btn btn-grad" href="#!">Get Involved</a>
 				</div>
 			</div>
 		</div>
 	</section>
 	<!-- =======================
-	Team -->
+Team -->
 
 
 
 
-    <!-- Footer, CTA -->
-    <?php include 'includes/footer.php'; ?>
+	<!-- Footer, CTA -->
+	<?php include 'includes/footer.php'; ?>
 
 
-    <!-- Scripts -->
-    <?php include 'includes/scripts.php'; ?>
+	<!-- Scripts -->
+	<?php include 'includes/scripts.php'; ?>
 
 
 </body>
