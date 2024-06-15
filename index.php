83117953a0b715d949fb5aec33e37ceb8f0e22ed
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-15 03:46:07 +0300

    modified the main banner

--- index.php
+++ index.php
@@ -24,26 +24,26 @@ Main Banner -->
         <div class="tiny-slider arrow-dark arrow-large arrow-transparent arrow-hover">
             <div class="tiny-slider-inner h-400 h-lg-700" data-autoplay="true" data-autoplaytime="7000" data-gutter="0" data-arrow="true" data-dots="false" data-items="1">
                 <!-- slide 1-->
-                <div class="h-100 bg-overlay-dark-2" style="background-image:url(assets/images/banner/05.jpg); background-position: center center; background-size: cover;">
+                <div class="h-100 bg-overlay-dark-2" style="background-image:url(assets/images/banner.jpg); background-position: center center; background-size: cover;">
                     <div class="container h-100">
                         <div class="row d-flex h-100">
                             <div class="col-lg-8 col-xl-6 me-auto slider-content justify-content-center align-self-center align-items-start text-start">
                                 <h2 class="animate__animated animate__fadeInUp animate__delay-1s display-2 fw-bold text-white">Revolutionizing Climate Resilience in Africa</h2>
                                 <h3 class="animate__animated animate__fadeInUp animate__delay-2s text-white display-7 alt-font fst-italic mb-2 my-md-4">Empowering small-scale producers through technology and carbon markets</h3>
-                                <div class="animate__animated animate__fadeInUp animate__delay-3s mt-3 dealy-1500"><a href="#" class="btn btn-grad">Access Carbon Markets</a> <a href="#" class="btn btn-link text-white">Learn More</a></div>
+                                <div class="animate__animated animate__fadeInUp animate__delay-3s mt-3 dealy-1500"><a href="#" class="btn btn-grad">Apply for Carbon Credits</a> <a href="login.php" class="btn btn-link text-white">Learn More</a></div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- slide 2-->
-                <div class="h-100 bg-overlay-dark-2" style="background-image:url(assets/images/banner/02.jpg); background-position: center top; background-size: cover;">
+                <div class="h-100 bg-overlay-dark-2" style="background-image:url(assets/images/banner2.jpg); background-position: center top; background-size: cover;">
                     <div class="container h-100">
                         <div class="row d-flex h-100">
                             <div class="col-md-8 justify-content-center align-self-center align-items-start mx-auto">
                                 <div class="slider-content text-center">
                                     <h3 class="animate__animated animate__fadeInUp animate__delay-1s display-7 text-white alt-font fst-italic">Driving sustainable development through innovative climate solutions</h3>
                                     <h2 class="animate__animated animate__fadeInUp animate__delay-2s display-3 fw-bold text-white">Join us in the fight against climate change</h2>
-                                    <div class="animate__animated animate__fadeInUp animate__delay-3s mt-3 dealy-1500"><a href="#" class="btn btn-grad">Get Involved</a></div>
+                                    <div class="animate__animated animate__fadeInUp animate__delay-3s mt-3 dealy-1500"><a href="contact.php#contact" class="btn btn-grad">Partner with Us</a></div>
                                 </div>
                             </div>
                         </div>
