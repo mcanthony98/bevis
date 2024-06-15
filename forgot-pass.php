293c3dd970bd4c710ab5923bb50ea8fd58c5adb1
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-15 05:11:17 +0300

    add forgot password page

---
+++ forgot-pass.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Login | Bevis Africa</title>
+
+    <!-- Head Content -->
+    <?php include 'includes/head-content.php'; ?>
+
+
+</head>
+
+<body>
+    <div class="preloader">
+        <img src="assets/images/preloader.svg" alt="Pre-loader">
+    </div>
+
+    <!-- =======================
+Sign in -->
+    <section class="p-0 d-flex align-items-center">
+        <div class="container-fluid">
+            <div class="row">
+                <!-- left -->
+                <div class="col-12 col-md-5 col-lg-4 d-md-flex align-items-center vh-sm-100" style="background-color: #2bb673;">
+                    <div class="w-100 p-3 p-lg-5 all-text-white">
+                        <div class="justify-content-center align-self-center">
+                            <!-- Image Logo -->
+                            <img src="assets/images/bevis-logo.jpg" alt="Logo" class="logo-auth mb-4">
+                        </div>
+                        <h3 class="fw-light">Empowering Africa's small-scale producers through carbon markets</h3>
+                        <ul class="list-group list-group-borderless mt-4">
+                            <li class="list-group-item text-white"><i class="fa fa-check"></i>Access the voluntary carbon market</li>
+                            <li class="list-group-item text-white"><i class="fa fa-check"></i>Contribute to climate change mitigation</li>
+                            <li class="list-group-item text-white"><i class="fa fa-check"></i>Enhance your resilience to climate change</li>
+                        </ul>
+                    </div>
+                </div>
+
+
+            </div>
+        </div>
+    </section>
+    <!-- =======================
+Sign in -->
+
+
+    <!-- Scripts -->
+    <?php include 'includes/scripts.php'; ?>
+
+
+</body>
+
+</html>
