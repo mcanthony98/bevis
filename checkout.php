725ba16b00dd50a8683815da34e0da013748c2f0
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-15 06:25:00 +0300

    add checkout page

---
+++ checkout.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Checkout | Bevis Africa</title>
+
+    <!-- Head Content -->
+    <?php include 'includes/head-content.php'; ?>
+
+
+</head>
+
+<body>
+    <div class="preloader">
+        <img src="assets/images/preloader.svg" alt="Pre-loader">
+    </div>
+
+    <!-- Header -->
+    <?php include 'includes/navbar.php'; ?>
+
+
+
+
+
+    <!-- Footer, CTA -->
+    <?php include 'includes/footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <?php include 'includes/scripts.php'; ?>
+
+
+</body>
+
+</html>
