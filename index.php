516247722115a145bf222151c521938924d7ce3d
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-15 03:33:17 +0300

    update the homepage

--- index.php
+++ index.php
@@ -19,42 +19,41 @@
     <?php include 'includes/navbar.php'; ?>
 
     <!-- =======================
-	Main Banner -->
-    <section class="p-0 position-relative h-500 h-md-600 h-xl-900 bg-grad pattern-overlay-1 overflow-hidden">
-        <div class="container d-flex h-100">
-            <div class="row align-self-center w-100">
-                <!-- banner Content -->
-                <div class="col-md-6 mt-md-0 all-text-white position-relative">
-                    <h2 class="display-4 fw-normal">Your Trusted Engineering Partner in Africa</h2>
-                    <p class="h6 mb-4 fw-normal d-none d-sm-block">Bevis Africa delivers world-class engineering, procurement, construction and maintenance services across the continent.</p>
-                    <a class="btn btn-dark mb-7" href="#">Learn More</a>
+Main Banner -->
+    <section class="p-0">
+        <div class="tiny-slider arrow-dark arrow-large arrow-transparent arrow-hover">
+            <div class="tiny-slider-inner h-400 h-lg-700" data-autoplay="true" data-autoplaytime="7000" data-gutter="0" data-arrow="true" data-dots="false" data-items="1">
+                <!-- slide 1-->
+                <div class="h-100 bg-overlay-dark-2" style="background-image:url(assets/images/banner/05.jpg); background-position: center center; background-size: cover;">
+                    <div class="container h-100">
+                        <div class="row d-flex h-100">
+                            <div class="col-lg-8 col-xl-6 me-auto slider-content justify-content-center align-self-center align-items-start text-start">
+                                <h2 class="animate__animated animate__fadeInUp animate__delay-1s display-2 fw-bold text-white">Revolutionizing Climate Resilience in Africa</h2>
+                                <h3 class="animate__animated animate__fadeInUp animate__delay-2s text-white display-7 alt-font fst-italic mb-2 my-md-4">Empowering small-scale producers through technology and carbon markets</h3>
+                                <div class="animate__animated animate__fadeInUp animate__delay-3s mt-3 dealy-1500"><a href="#" class="btn btn-grad">Access Carbon Markets</a> <a href="#" class="btn btn-link text-white">Learn More</a></div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <!-- banner image -->
-                <div class="col-md-6 d-none d-md-block p-0 position-absolute top-0 end-0 align-top align-text-top">
-                    <figure class="w-100 ie-height-750">
-                        <svg version="1.1" id="Layer_2" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 1290 1024" style="enable-background:new 0 0 1290 1024;" xml:space="preserve">
-                            <g id="XMLID_81_">
-                                <defs>
-                                    <path id="XMLID_3_" d="M1290,0v1024c0,0-229.8-152.2-550-52.9C467.3,1055.7-22.5,927.6,47.8,540C120.4,139.6,17,0,17,0H1290z" />
-                                </defs>
-                                <clipPath id="XMLID_83_">
-                                    <use xlink:href="#XMLID_3_" style="overflow:visible;" />
-                                </clipPath>
-                                <g style="clip-path:url(#XMLID_83_);">
-                                    <!-- Set your 1290px X 1024px image below -->
-                                    <image style="overflow:visible;" width="1290" height="1024" id="XMLID_82_" xlink:href="assets/images/banner/10.jpg" transform="matrix(0.9999 0 0 0.9999 8.063897e-002 6.401104e-002)">
-                                    </image>
-                                </g>
-                            </g>
-                        </svg>
-                    </figure>
+                <!-- slide 2-->
+                <div class="h-100 bg-overlay-dark-2" style="background-image:url(assets/images/banner/02.jpg); background-position: center top; background-size: cover;">
+                    <div class="container h-100">
+                        <div class="row d-flex h-100">
+                            <div class="col-md-8 justify-content-center align-self-center align-items-start mx-auto">
+                                <div class="slider-content text-center">
+                                    <h3 class="animate__animated animate__fadeInUp animate__delay-1s display-7 text-white alt-font fst-italic">Driving sustainable development through innovative climate solutions</h3>
+                                    <h2 class="animate__animated animate__fadeInUp animate__delay-2s display-3 fw-bold text-white">Join us in the fight against climate change</h2>
+                                    <div class="animate__animated animate__fadeInUp animate__delay-3s mt-3 dealy-1500"><a href="#" class="btn btn-grad">Get Involved</a></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
-        <canvas class="position-absolute bottom-0 start-50 translate-middle-x mb-n9" id="waveCanvas"></canvas>
     </section>
     <!-- =======================
-	Main banner -->
+Main banner -->
 
 
     <!-- =======================
