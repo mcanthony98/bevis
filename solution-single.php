a1ce309075c7b6e1fda15669966c506822ac49c8
Tonny G <118627853+gitongatonny@users.noreply.github.com>
2024-06-28 03:47:21 +0300

    added a single solution page

---
+++ solution-single.php
@@ -0,0 +1,92 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Sample Page | Bevis Africa</title>
+
+    <!-- Head Content -->
+    <?php include 'includes/head-content.php'; ?>
+
+
+</head>
+
+<body>
+    <div class="preloader">
+        <img src="assets/images/preloader.svg" alt="Pre-loader">
+    </div>
+
+    <!-- Header -->
+    <?php include 'includes/navbar.php'; ?>
+
+    <!-- =======================
+	Banner innerpage -->
+    <div class="left bg-dark-grad pattern-overlay-1">
+        <div class="container">
+            <div class="row all-text-white">
+                <div class="col-md-12 align-self-center position-relative">
+                    <h1 class="fw-bold display-1 mb-2 mb-md-n4 mt-9">Solution 1</h1>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- =======================
+	Banner innerpage -->
+
+    <!-- =======================
+	info -->
+    <section class="">
+        <div class="container">
+            <div class="row">
+                <!-- left -->
+                <div class="col-md-6 pe-lg-5">
+                    <!-- content -->
+                    <div class="mt-5 mt-md-0">
+                        <p class="bg-dark small text-white rounded px-2 py-0 mb-2 d-inline-block">Marketing</p>
+                        <h2 class="h1 fw-bold">Engaging, purposeful, and creative.</h2>
+                        <div class="d-flex mt-3">
+                            <h6 class="display-1 opacity-2 me-3 align-self-start fw-bold">01</h6>
+                            <div class="align-self-start">
+                                <p>A thing that looks beautiful and attractive always grabs eyeballs. The real challenge before companies in today's world is to make their products and services appealing to everyone. thing that looks beautiful and attractive always grabs eyeballs.</p>
+                                <a class="primary-hover" href="#">Read More <i class="fa fa-long-arrow-right ms-1"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- image -->
+                    <img class="rounded mt-4 shadow-hover up-on-hover" src="assets/images/service/01.jpg" alt="">
+                </div>
+
+                <!-- right -->
+                <div class="col-md-6 ps-lg-5 mt-5 mt-md-0">
+                    <!-- image -->
+                    <img class="rounded mb-4 shadow-hover up-on-hover" src="assets/images/service/02.jpg" alt="">
+                    <!-- content -->
+                    <div>
+                        <p class="bg-dark small text-white rounded px-2 py-0 mb-2 d-inline-block">Advertising</p>
+                        <h2 class="h1 fw-bold">Expect nothing less than perfect.</h2>
+                        <div class="d-flex mt-3">
+                            <h6 class="display-1 opacity-2 me-3 align-self-start fw-bold">02</h6>
+                            <div class="align-self-start">
+                                <p>We have a strong foundation built on legacy and emerging technologies, including excellent track record of on-time deliveries. We are emerging as one of the most promising private talent sourcing and management firms in the world.</p>
+                                <a class="primary-hover" href="#">Read More <i class="fa fa-long-arrow-right ms-1"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+    <!-- Footer, CTA -->
+    <?php include 'includes/footer.php'; ?>
+
+
+    <!-- Scripts -->
+    <?php include 'includes/scripts.php'; ?>
+
+
+</body>
+
+</html>
