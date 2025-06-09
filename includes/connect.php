b84e8145bbdc5716f4564efa480eaa3eb34dc149
mcanthony98 <36364125+mcanthony98@users.noreply.github.com>
2025-06-09 19:35:16 +0300

    Update connect.php

--- includes/connect.php
+++ includes/connect.php
@@ -1,8 +1,18 @@
 <?php
-define('DB_SERVER', 'srv497.hstgr.io');
+
+define('DB_SERVER', 'srv491.hstgr.io');
 define('DB_USERNAME', 'u640333703_bevis');
 define('DB_PASSWORD', '?3wm~Agt?#pN');
 define('DB_DATABASE', 'u640333703_bevis');
+
+/*
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'bevisafr_bevis');
+define('DB_PASSWORD', '?3wm~Agt?#pN');
+define('DB_DATABASE', 'bevisafr_GNM');
+*/
 $conn = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
 $pdo = $conn;
+
+
 ?>
